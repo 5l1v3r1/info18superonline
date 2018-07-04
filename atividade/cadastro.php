ca5a78ce017d9113a054095af616b6e6dae39208
Isabelly_Oliveira <isabelly.euflauzino@gmail.com>
2018-07-04 15:09:15 -0300

    Enviando tela cadastro

---
+++ atividade/cadastro.php
@@ -0,0 +1,23 @@
+
+<?php
+
+$nome=$_POST['nome'];
+$email=$_POST['email'];
+$usu=$_POST['usuario'];
+$sen=$_POST['senha'];
+$s2=$_POST['senha2'];
+echo $nome."</br>";
+echo $email."</br>";
+echo $usu."</br";
+echo $sen."</br>";
+echo $s2."</br>";
+
+
+
+
+
+
+?>
+
+
+
