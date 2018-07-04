c94ea390dc398131630944675a9ab8e9e7738da8
Thais Vieira <thaishellena88@gmail.com>
2018-07-04 14:57:51 -0300

    Enviando tela de login

---
+++ atividade/logo.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<title> pagina php</title>
+</head>
+<body>
+<?php
+$v1= $_POST["email1"];
+$v2= $_POST["senha1"];
+if($v1=="adim" && $v2=="123"){
+	echo("Parabéns, você foi cadastrado.");
+	
+	
+}
+
+
+?>
+
+
+
+</body>
+</html>
