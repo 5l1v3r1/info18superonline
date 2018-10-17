7f16e80ccf6074dbf14e847953f3dd555b674867
Isabelly de Oliveira Euflauzino <isabelly.oliveira@academico.ifrn.edu.br>
2018-10-17 16:38:01 -0300

    Isabelly

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Principais categorias</h1>
+		 <p>Os melhores preços e produtos voce encontra aqui na SUPER.ONLINE!</p>
+		<br/>
+
+		<div class="row">
+			
+			<div class="col-md-4">
+			<div class="gallery"> <a>
+				<a href="https://target.scene7.com/is/image/Target/14930889?wid=1560&hei=1560&fmt=pjpeg"><img src="https://target.scene7.com/is/image/Target/14930889?wid=1560&hei=1560&fmt=pjpeg" class="img-responsive" style="width:70%" alt="Image"></a>
+				 <h4> SALGADINHO DORITOS </h4></br>
+			        <h4>R$ 5,00</h4>
+				 </a>
+				</div>
+				</div>
+				
+			
+			<div class="col-md-4">
+				<a href="dados_prato.html"><img src="https://www.deliveryextra.com.br/img/uploads/1/800/527800.jpg?type=product" class="img-responsive" style="width:70%"  alt="Image"></a>
+			<h4> CAIXA DE CHOCOLATE NESTLE </h4></br>
+			        <h4>R$ 10,00</h4>
+			</div>
+			<div class="col-md-4">
+				<a href="dados_prato.html"><img src="http://coopsp.vteximg.com.br/arquivos/ids/157238-804-804/7894900011593_Refrigerante-Coca-Cola---25L.jpg?v=636136227943470000" class="img-responsive" style="width:70%" alt="Image"></a>
+				<h4> REFRIGERANTE COCA COLA </h4></br>
+			        <h4>R$ 6,00</h4>
+				</a>
+			</div>
+		</div>
+
+		<br/>
+
+		<div class="row">
+			<div class="col-md-4">
+				<a href="dados_prato.html"><img src="http://www.saobraz.com.br/static/img/produtos/cafe/cafe_extra_forte250g.png" class="img-responsive" style="width:50%" alt="Image"></a>
+                <h4> CAFE SAO BRAZ</h4></br>
+			        <h4>R$ 4,00</h4>                   
+				   </a>                 
+			</div>
+			<div class="col-md-4">
+				<a href="dados_prato.html"><img src="https://www.deliveryextra.com.br/img/uploads/1/757/539757.jpg?type=product" class="img-responsive" style="width:70%" alt="Image"></a>
+			    <h4> AMACIANTE CONFORT </h4></br>
+			        <h4>R$ 5,00</h4>
+			</a>
+			</div>
+			<div class="col-md-4">
+				<a href="dados_prato.html"><img src="http://4.bp.blogspot.com/-ViZJK_1bMOk/VX-pN76XJYI/AAAAAAAAGFA/Jb8J0ogaPKw/s1600/Pipoca%2BPronta%2B-%2Bsabor%2BCaramelo_50g.jpg" class="img-responsive" style="width:60%" alt="Image"></a>
+			    <h4> PIPOCA PRONTA YOKI </h4></br>
+			        <h4>R$ 3,00</h4>
+			</a>
+			</div>
+		</div>
+
+		<hr/>
+		 
+		
+                    
+                        <h1>Produtos em Destaque</h1>
+                       
+                 
+		
+		<br/>
+
+		<div class="row">
+			<div class="col-md-4">
+				<a href="dados_prato.html"><img src="https://ikesaki.vteximg.com.br/arquivos/ids/191731-1000-1000/Kit-Shampoo---Condicionador-Tresemme-200ml--55--Desconto-no-Condicionador-.jpg?v=636245751189800000" class="img-responsive" style="width:70%" alt="Image"></a>
+			    <h4> SHAMPOO TRESEMME </h4></br>
+			        <h4>R$ 10,00</h4>
+			</a>
+			</div>
+			<div class="col-md-4">
+				<a href="dados_prato.html"><img src="https://savegnago.vteximg.com.br/arquivos/ids/278422-1000-1000/CERVEJA-BRAHMA-350ML-LATA-1.jpg?v=636259642802870000" class="img-responsive" style="width:70%" alt="Image"></a>
+			    <h4> CERVEJA BRAHMA 350ML-LATA </h4></br>
+			        <h4>R$ 5,00</h4>
+			</a>
+			</div>
+			<div class="col-md-4">
+				<a href="dados_prato.html"><img src="https://carrinho.extra.com.br/bebes/TrocadoBebe/FraldasDescartaveisparaBebes/2221846/11239279/Fralda-Huggies-com-Turma-da-Monica-Supreme-Care-Meninos-Hiper-G-64-Unidades-2221846.jpg" class="img-responsive" style="width:70%" alt="Image"></a><br>
+		        <h4> FRALDA TURMA DA MONICA </h4></br>
+			        <h4>R$ 6,00</h4>
+		<a/>
+		</div>
+
+			
+
+
+	</div>
+
+	
+</div>
+@yield('conteudo')
+
+</body>
+</html>
